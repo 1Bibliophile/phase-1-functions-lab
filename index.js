5ea2c849615618a75ce8bf0618529fd5a69da9d8
David Morgenstern <DavidtheConfident@gmail.com>
2021-09-29 21:23:48 -0500

    commit tree 71abd237434e4fa6ca60c0a2a8629b7999a9017c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dist){
+    let distance = Math.abs(dist - 42)
+    return distance
+}
+
+function distanceFromHqInFeet(blocks){
+    //distanceFromHqInBlocks(blocks)
+    let feet = Math.abs(blocks - 42) * 264
+    return feet
+}
+
+function distanceTravelledInFeet(num1, num2){
+    let foot = Math.abs((num1 - num2) * 264)
+    return foot
+}
+
+function calculatesFarePrice(start, destination){
+    let dist2 = Math.abs(start - destination) * 264
+    let price = 0
+    let tooFar = 'cannot travel that far'
+    if(dist2 < 400)
+    {
+        price = 0
+        return price
+    }
+    else if(dist2 > 400 && dist2 < 2000)
+    {
+        price = Math.abs((dist2 - 400) * 0.02)
+        return price
+    }
+    else if(dist2 > 2000 && dist2 < 2500)
+    {
+        price = 25
+        return price
+    }
+    else if(dist2 > 2500)
+    {
+        return tooFar
+    }
+    
+}
